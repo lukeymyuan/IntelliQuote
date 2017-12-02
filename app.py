889c497e34f61fa1c86ed08382520c1bf55fb711
Naman Shenoy <naman.skillet@gmail.com>
2017-12-02 18:11:53 +0000

    csv generator

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,4 +10,4 @@ def plans():
 	return render_template("plans.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
---
+++ data_generator.py
@@ -0,0 +1,40 @@
+import pymongo
+import requests
+import csv
+
+client = pymongo.MongoClient("mongodb://admin1:Naman123@localhost:27017/admin")
+def split_date(date):
+    splot = date.split("T")
+    return[splot[0].split('-')[0], splot[0].split('-')[1]]
+
+def quote_to_number(quote_type):
+    if quote_type == "Bronze": return 0
+    if quote_type == "Silver": return 1
+    if quote_type == "Gold": return 2
+    if quote_type == "Platinum": return 3
+    return -1
+def worker(filename):
+    participant = requests.get("https://v3v10.vitechinc.com/solr/v_participant/select?q=*:*&sort=id asc&rows=1000&wt=json").json()['response']['docs']
+    participant_details = requests.get("https://v3v10.vitechinc.com/solr/v_participant_detail/select?q=*:*&sort=id asc&rows=1000&wt=json").json()['response']['docs']
+    quotes = requests.get("https://v3v10.vitechinc.com/solr/v_quotes/select?q=*:*&sort=id asc&rows=1000&wt=json").json()['response']['docs']
+    for index in range(len(participant)):
+        print(participant[index])
+        print(participant_details[index])
+        print(quotes[index])
+        assert participant[index]['id'] == participant_details[index]['id'], "Unordered!"
+        assert participant[index]['id'] == quotes[index]['id'], exit(1)
+
+    with open(filename, 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar="'", quoting=csv.QUOTE_MINIMAL)
+        for index in range(len(participant)):
+            #preconditions = participant_details[index]['preconitions']
+            #preconditions_json = json.parse(preconditions)
+
+            writer.writerow([participant[index]['id']]+[participant[index]['state']]+[participant[index]['sex']]+\
+                            [split_date(participant[index]['DOB'])[0]]+[split_date(participant[index]['DOB'])[1]]\
+                            +[1 if participant_details[index]['TOBACCO'] == "Yes" else 0] + [participant_details[index]['ANNUAL_INCOME']] + [participant_details[index]['OPTIONAL_INSURED']] \
+                            + [participant_details[index]['WEIGHT']]+[participant_details[index]['HEIGHT']]+[0 if participant_details[index]['MARITAL_STATUS'] == "S" else 1] \
+                            +[quotes[index]['GOLD']]+[quotes[index]['SILVER']]+[quotes[index]['BRONZE']]+[quote_to_number(quotes[index]['PURCHASED'])]\
+                            )
+if __name__ == '__main__':
+    worker('test.csv')
