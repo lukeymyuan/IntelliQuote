59b54044e6b0e94550b2a0719a0a061c78402f82
Luke <lukelalalala@gmail.com>
2017-12-02 00:42:59 -0500

    first version front end

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-        return 'Hello, World!'
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/plans")
+def plans():
+	return render_template("plans.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
